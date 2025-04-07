2e29d47429d5fb9d7f7a24771d5ae972fc36ae8d
financegeek-org <nlai@rediffmail.com>
2025-04-07 12:57:41 -0700

    Revert "generate map"
    This reverts commit b3cee5787fb8e4f7425d112d9a0a33d2d0889664.

--- src/simulators/plugins/__init__.py
+++ src/simulators/plugins/__init__.py
@@ -1,7 +1,1 @@
 # This can be empty
-
-# Import and expose the mapping plugin
-from src.simulators.plugins.SimpleMapper import SimpleMapper
-from src.simulators.plugins.MapGeneratorPlugin import MapGeneratorPlugin
-
-__all__ = ['SimpleMapper', 'MapGeneratorPlugin']
